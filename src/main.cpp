379127bc1a04f58564d16cebaef74d27ca8226cf
bub655 <52846238+bub655@users.noreply.github.com>
2023-12-22 12:08:44 -0800

    bay area battle pros lemlib + driver control

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,28 @@
 #include "main.h"
 #include "lemlib/api.hpp"
 #include <string>
+using namespace pros;
 
-pros::Controller controller(pros::E_CONTROLLER_MASTER);
-pros::Motor motor_lf(4, pros::E_MOTOR_GEARSET_06, true);
-pros::Motor motor_lb(5, pros::E_MOTOR_GEARSET_06, true);
-pros::Motor motor_lt(6, pros::E_MOTOR_GEARSET_06, false);
-pros::Motor motor_rf(7, pros::E_MOTOR_GEARSET_06, true);
-pros::Motor motor_rb(8, pros::E_MOTOR_GEARSET_06, true);
-pros::Motor motor_rt(9, pros::E_MOTOR_GEARSET_06, false);
-pros::Motor intake(10);
-pros::Motor flywheel(13);
-pros::Motor_Group leftDrive({motor_lf, motor_lb, motor_lt});
-pros::Motor_Group rightDrive({motor_rf, motor_rb, motor_rt});
-pros::ADIDigitalOut lift('A');
-pros::ADIDigitalOut hang('B');
-pros::ADIDigitalOut wings('C');
-pros::Imu iner(15);
+Controller controller(E_CONTROLLER_MASTER);
+Motor motor_lf(4, E_MOTOR_GEARSET_06, true);
+Motor motor_lb(5, E_MOTOR_GEARSET_06, true);
+Motor motor_lt(6, E_MOTOR_GEARSET_06, false);
+Motor motor_rf(7, E_MOTOR_GEARSET_06, false);
+Motor motor_rb(8, E_MOTOR_GEARSET_06, false);
+Motor motor_rt(10, E_MOTOR_GEARSET_06, true);
+Motor intake(14);
+Motor flywheel(11, true);
+Motor_Group leftDrive({motor_lf, motor_lb, motor_lt});
+Motor_Group rightDrive({motor_rf, motor_rb, motor_rt});
+ADIDigitalOut lift('A');
+ADIDigitalOut hang('B');
+ADIDigitalOut wings('C');
+Imu iner(13);
 
 lemlib::Drivetrain_t drivetrain{
 		&leftDrive,	 // left drivetrain motors
 		&rightDrive, // right drivetrain motors
-		10,					 // track width
+		11.4,				 // track width
 		3.25,				 // wheel diameter
 		400					 // wheel rpm
 };
@@ -35,8 +36,8 @@ lemlib::OdomSensors_t sensors{
 };
 
 lemlib::ChassisController_t lateralController{
-		0.8, // kP
-		3,	 // kD
+		0.1, // kP
+		0.1, // kD
 		1,	 // smallErrorRange
 		100, // smallErrorTimeout
 		3,	 // largeErrorRange
@@ -45,13 +46,13 @@ lemlib::ChassisController_t lateralController{
 };
 
 lemlib::ChassisController_t angularController{
-		0.8, // kP
-		3,	 // kD
+		2,	 // kP
+		1,	 // kD
 		1,	 // smallErrorRange
 		100, // smallErrorTimeout
 		3,	 // largeErrorRange
 		500, // largeErrorTimeout
-		0		 // slew rate
+		5		 // slew rate
 };
 
 lemlib::Chassis chassis(drivetrain, lateralController, angularController, sensors);
@@ -66,12 +67,11 @@ void screen()
 	// loop forever
 	while (true)
 	{
-		pros::Controller master(pros::E_CONTROLLER_MASTER);
-		lemlib::Pose pose = chassis.getPose();													// get the current position of the robot
-		controller.set_text(0, 0, "x: " + to_string(pose.x));						// print the x position
-		controller.set_text(1, 0, "y: " + to_string(pose.y));						// print the y position
-		controller.set_text(2, 0, "heading: " + to_string(pose.theta)); // print the heading
-		pros::delay(70);																								// delay for 20 milliseconds
+		lemlib::Pose pose = chassis.getPose();		// get the current position of the robot
+		lcd::print(0, "x: %f", pose.x);						// print the x position
+		lcd::print(1, "y: %f", pose.y);						// print the y position
+		lcd::print(2, "heading: %f", pose.theta); // print the heading
+		delay(10);
 	}
 }
 
@@ -79,10 +79,18 @@ void screen()
 // All other competition modes are blocked by initialize; it is recommended to keep execution time for this mode under a few seconds.
 void initialize()
 {
-	pros::lcd::initialize();
+	lcd::initialize();
+	motor_lb.set_brake_mode(E_MOTOR_BRAKE_COAST);
+	motor_lf.set_brake_mode(E_MOTOR_BRAKE_COAST);
+	motor_lt.set_brake_mode(E_MOTOR_BRAKE_COAST);
+	motor_rb.set_brake_mode(E_MOTOR_BRAKE_COAST);
+	motor_rf.set_brake_mode(E_MOTOR_BRAKE_COAST);
+	motor_rt.set_brake_mode(E_MOTOR_BRAKE_COAST);
+	flywheel.set_brake_mode(E_MOTOR_BRAKE_COAST);
+	intake.set_brake_mode(E_MOTOR_BRAKE_HOLD);
 	chassis.calibrate();
 	chassis.setPose(0, 0, 0);
-	controller.clear();
+	Task screenTask();
 }
 
 // Runs when robot is in disabled state of Field Management System or VEX Competition Switch, following either autonomous or opcontrol. When the robot is enabled, this task will exit.
@@ -99,6 +107,79 @@ void disabled() {}
  */
 void competition_initialize() {}
 
+void skills()
+{
+	// flywheel.move(115);
+	// leftDrive.move(-127);
+	// rightDrive.move(127);
+	// delay(100);
+	// leftDrive.move(0);
+	// rightDrive.move(0);
+	// delay(36000);
+	chassis.setPose(-48.95579437229437, -59.557686147186146, 45);
+	chassis.follow("skills_p1.txt", 4500, 12);
+	leftDrive.move(127);
+	rightDrive.move(127);
+	delay(600);
+	leftDrive.move(-127);
+	rightDrive.move(-127);
+	delay(100);
+	chassis.follow("skills_p2.txt", 4000, 10, true);
+	chassis.follow("skills_p3.txt", 1000, 10);
+	chassis.follow("skills_p4.txt", 4000, 10, true);
+	chassis.follow("skills_p5.txt", 1500, 10);
+	chassis.follow("skills_p6.txt", 5000, 10, true);
+	leftDrive.move(-127);
+	rightDrive.move(-127);
+	delay(800);
+	leftDrive.move(0);
+	rightDrive.move(0);
+}
+
+void nearSide()
+{
+	chassis.setPose(-43.135147907647905, -54.152800144300144, 20);
+	intake.move(127);
+	chassis.follow("near_p1.txt", 2500, 12);
+	// leftDrive.move(-127);
+	// delay(500);
+	chassis.follow("near_p2.txt", 2800, 15, true);
+	intake.move(0);
+	wings.set_value(1);
+	delay(250);
+	leftDrive.move(-127);
+	rightDrive.move(127);
+	delay(330);
+	wings.set_value(0);
+	chassis.follow("near_p3.txt", 3000, 10, true);
+	leftDrive.move(50);
+	rightDrive.move(50);
+	delay(100);
+	leftDrive.move(0);
+	rightDrive.move(0);
+	// leftDrive.move(127);
+	// rightDrive.move(127);
+	// delay(200);
+	// leftDrive.move(-127);
+	// rightDrive.move(-127);
+	// delay(100);
+	// chassis.follow("near_4.txt", 2000, 10, true);
+}
+
+void farSide()
+{
+	leftDrive.move(-127);
+	rightDrive.move(-127);
+	delay(3000);
+	// leftDrive.move(127);
+	// rightDrive.move(127);
+	// delay(1000);
+	// leftDrive.move(-127);
+	// rightDrive.move(-127);
+	// delay(3000);
+	leftDrive.move(0);
+	rightDrive.move(0);
+}
 /**
  * Runs the user autonomous code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -110,10 +191,10 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
+
 void autonomous()
 {
-	pros::Task screenTask(screen);
-	chassis.turnTo(30, 0, 2000);
+	skills();
 }
 
 /**
@@ -133,61 +214,51 @@ void autonomous()
 void opcontrol()
 {
 	float power, turn;
-	bool buttonR1Pressed, buttonR2Pressed, buttonYPressed, buttonXPressed, buttonRightPressed;
+	bool buttonR1Pressed, buttonR2Pressed, buttonYPressed, buttonXPressed, buttonL2Pressed, buttonAPressed;
 	bool flywheelOn, flywheelPressed = false;
 	bool toggleLift, liftPressed = false;
 	bool toggleHang, hangPressed = false;
-
-	motor_lb.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-	motor_lf.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-	motor_lt.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-	motor_rb.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-	motor_rf.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-	motor_rt.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-	flywheel.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-	intake.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-
-	controller.set_text(0, 0, "Example");
-	pros::delay(100);
-	controller.clear();
-
+	bool toggleWings, wingsPressed = false;
 	while (true)
 	{
-		pros::Task screenTask(screen);
+		// Task screenTask(screen);
 		// Get joystick values
-		power = controller.get_analog(ANALOG_LEFT_X);
-		turn = controller.get_analog(ANALOG_LEFT_Y);
+		turn = 0.5 * (controller.get_analog(E_CONTROLLER_ANALOG_LEFT_X));
+		power = controller.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
 
 		// Check if buttons are pressed
-		buttonR1Pressed = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
-		buttonR2Pressed = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
-		buttonYPressed = controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y);
-		buttonXPressed = controller.get_digital(pros::E_CONTROLLER_DIGITAL_X);
-		buttonRightPressed = controller.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT);
-
+		buttonR1Pressed = controller.get_digital(E_CONTROLLER_DIGITAL_R1);
+		buttonR2Pressed = controller.get_digital(E_CONTROLLER_DIGITAL_R2);
+		buttonYPressed = controller.get_digital(E_CONTROLLER_DIGITAL_Y);
+		buttonXPressed = controller.get_digital(E_CONTROLLER_DIGITAL_X);
+		buttonL2Pressed = controller.get_digital(E_CONTROLLER_DIGITAL_L2);
+		buttonAPressed = controller.get_digital(E_CONTROLLER_DIGITAL_A);
 		// Set motor velocities based on joystick inputs and button state
-		leftDrive.move(6 * (power + turn) / 1.27);
-		rightDrive.move(6 * (power - turn) / 1.27);
-
+		leftDrive.move((turn + power));
+		rightDrive.move((power - turn));
 		// Set intake motor velocity based on button state
 		if (buttonR1Pressed == 1)
 		{
-			intake.move(600);
+			intake.move(127);
 		}
 		else if (buttonR2Pressed == 1)
 		{
-			intake.move(-600);
+			intake.move(-127);
 		}
 		else
 		{
-			intake.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 			intake.brake();
+		}
+
+		if (buttonL2Pressed)
+		{
+			flywheel.move(-127);
 		}
 
 		// Toggle flywheel based on button state
 		if (flywheelOn)
 		{
-			flywheel.move(600);
+			flywheel.move(115);
 		}
 		else
 		{
@@ -208,7 +279,7 @@ void opcontrol()
 		}
 
 		// Enable hang if arrow key is pressed
-		if (buttonRightPressed)
+		if (buttonL2Pressed)
 		{
 			if (!hangPressed)
 			{
@@ -253,6 +324,28 @@ void opcontrol()
 			lift.set_value(0);
 		}
 
-		pros::delay(20); // Typical delay in an opcontrol loop
+		// toggle wings
+		if (buttonAPressed)
+		{
+			if (!wingsPressed)
+			{
+				toggleWings = !toggleWings;
+				wingsPressed = true;
+			}
+		}
+		else
+		{
+			wingsPressed = false;
+		}
+
+		if (toggleWings)
+		{
+			wings.set_value(1);
+		}
+		else
+		{
+			wings.set_value(0);
+		}
+		delay(20); // Typical delay in an opcontrol loop
 	}
 }
